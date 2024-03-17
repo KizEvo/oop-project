8fd1a874831003d1918b34c69add34aaeafda934
Duc Phu <vandungqc03@gmail.com>
2024-03-17 13:08:54 +0700

    Added Apartment class, fixed Tenant class
    - Apartment class : the attributes help the Landlord class (implement later) quickly identify the apartment ID, the owner name, the rent dates etc
    - Tenant class : implemented "has-a" and "is-a" relationship, Tenant class can be interpret this way : Tenant "is a" Person that "has-a" Apartment.

--- inc/tenant.h
+++ inc/tenant.h
@@ -2,20 +2,14 @@
 #define TENANT_H
 
 #include "person.h"
-
-enum PAYMENT_METHOD {CASH, DEBIT_CARD, CREDIT_CARD};
+#include "apartment.h"
 
 class Tenant: public Person
 {
         private:
-                PAYMENT_METHOD paymentMethod;
-                Date startRent;
-                Date endRent;
-
-        public:
-                /*Constructor*/
-                Tenant();
-                Tenant(PAYMENT_METHOD, Date, Date);
+        	Apartment apartment;
+	public:
+		const std::string &getApartmentId();
 };
 
 #endif
--- src/tenant.cpp
+++
@@ -1,7 +0,0 @@
-#include "date.h"
-#include "tenant.h"
-
-Tenant::Tenant() : paymentMethod{CASH}, startRent{}, endRent{} {}
-
-Tenant::Tenant(PAYMENT_METHOD method, Date startRentDate, Date endRentDate)
-		: paymentMethod{method}, startRent{startRentDate}, endRent{endRentDate} {}
